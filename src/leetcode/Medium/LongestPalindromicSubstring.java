397cf08ce49b2fce5f359950c8f1de6e1d436641
iflytang <iflytang@qq.com>
2017-11-06 22:25:58 +0800

    zigzag conversion

---
+++ src/leetcode/Medium/LongestPalindromicSubstring.java
@@ -0,0 +1,9 @@
+package leetcode.Medium;
+
+/**
+ * Created by tsf on 17-11-4.
+ */
+
+
+public class LongestPalindromicSubstring {
+}
---
+++ src/leetcode/Medium/ZigZagConversion.java
@@ -0,0 +1,43 @@
+package leetcode.Medium;
+
+/**
+ * Created by tsf on 17-11-6.
+ */
+
+
+public class ZigZagConversion {
+
+    public String convert(String s, int numRows) {
+        StringBuilder[]  temp = new StringBuilder[numRows];
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numRows; i++) {
+            temp[i] = new StringBuilder();
+        }
+
+        // store in temp Array
+        int i = 0;
+        while (i < s.length()) {
+            for (int index = 0; index < numRows && i < s.length(); index++) {
+                temp[index].append(s.charAt(i++));
+            }
+            for (int index = numRows - 2; index >= 1 && i < s.length(); index--) {
+                temp[index].append(s.charAt(i++));
+            }
+        }
+
+        // get the result
+        for (int index = 0; index < numRows; index++) {
+            //System.out.println(temp[index].toString());
+            result.append(temp[index]);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ZigZagConversion test = new ZigZagConversion();
+        String s = test.convert("ABC", 2);
+        System.out.println("the Zigzag conversion is: " + s);
+    }
+}
