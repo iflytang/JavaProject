b714d3bdff1fbe0c3169468455e77c779caee8cf
iflytang <iflytang@qq.com>
2017-11-18 14:49:26 +0800

    support no overlay id generator

--- src/javacore/RandomGenerator/RandomGenerator.java
+++ src/javacore/RandomGenerator/RandomGenerator.java
@@ -1,8 +1,6 @@
 package javacore.RandomGenerator;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by tsf on 17-11-17.
@@ -11,22 +9,39 @@ import java.util.Set;
 
 public class RandomGenerator {
     protected Set<Integer> ueIdSet = new HashSet<>();
+    protected List<Integer> ueIdList = new ArrayList<>();
+
 
     public int randomGeneratorByRandom() {
         Random random = new Random();
         int ueId = random.nextInt(10) + 1;
-//        System.out.println("random ueId by random: " + ueId);
-        forloop:
-        for(Integer assignedId : ueIdSet) {
+        int randRange = 10;
+        int i;
+        System.out.println("assign ueId in random: " + ueId);
+
+        if(ueIdList.size() == randRange) {
+            System.out.println("no more ueId! assign fails!");
+            return 255;
+        }
+
+        //forloop:
+        for(i = 0; i < ueIdList.size(); i ++) {
             // if assigned, reassign ueId and recheck
+            Integer assignedId = ueIdList.get(i);
+            System.out.println("assigned ueId in ueIdList ==> " + assignedId + ", i == > " + i);
             if(ueId == assignedId) {
                 int tempUeId = random.nextInt(10) + 1;
                 System.out.println("Warning! ueId conflicts! There have been ueId ==> " + ueId + ", reassign ueId ==> " + tempUeId);
                 ueId = tempUeId;
-                break forloop;
+                i = 0;
+                i--;
+               // break forloop;
             }
         }
         ueIdSet.add(ueId);
+        ueIdList.add(ueId);
+        Collections.sort(ueIdList);
+        System.out.println("ueId " + ueId + " store in ueIdSet " + ueIdSet + ", in ueIdList " + ueIdList);
         return ueId;
     }
 
@@ -39,9 +54,9 @@ public class RandomGenerator {
     public static void main(String[] args) {
         RandomGenerator test = new RandomGenerator();
         int ueId;
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 11; i++) {
             ueId = test.randomGeneratorByRandom();
-            System.out.println("assign [" + i + "] random ueId by random: " + ueId);
+            System.out.println("assign [" + i + "] random ueId by random: " + ueId + "\n");
 //            test.randomGeneratorByMath();
         }
     }
