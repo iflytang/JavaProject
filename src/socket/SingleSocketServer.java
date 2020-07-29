d3ff83b36afcd7f7637643c20e58c3c0b1f114e0
iflytang <iflytang@qq.com>
2020-07-29 17:52:43 +0800

    socket: receive predicted trace from DL

--- src/socket/SingleSocketServer.java
+++ src/socket/SingleSocketServer.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @author tsf
@@ -17,7 +20,7 @@ public class SingleSocketServer implements Runnable {
 
     private static final String SERVER_ADDR = "192.168.109.214";
     private static final String CLIENT_ADDR = "192.168.109.209";
-    private static final int PORT = 2019;
+    private static final int PORT = 2020;
 
     private static final short SHORT_LEN = 4;  // in hex str
     private static final short SHORT_OFF = 4;  // in hex str
@@ -27,6 +30,36 @@ public class SingleSocketServer implements Runnable {
 
     public SingleSocketServer(Socket client) {
         this.client = client;
+    }
+
+    public static double bytes2Double(byte[] arr, int k){
+        long value=0;
+        for(int i=0;i< 8;i++){
+            value|=((long)(arr[k]&0xff))<<(8*i);
+            k++;
+        }
+        return Double.longBitsToDouble(value);
+    }
+
+    public static int bytes2Int(byte[] arr, int k){
+        int value=0;
+        for(int i=0;i< 4;i++){
+            value|=((arr[k]&0xff))<<(4*i);
+            k++;
+        }
+        return value;
+    }
+
+    public static float bytes2float(byte[] b, int index) {
+        int l;
+        l = b[index + 0];
+        l &= 0xff;
+        l |= ((long) b[index + 1] << 8);
+        l &= 0xffff;
+        l |= ((long) b[index + 2] << 16);
+        l &= 0xffffff;
+        l |= ((long) b[index + 3] << 24);
+        return Float.intBitsToFloat(l);
     }
 
     public boolean parse (String msg) {
@@ -52,33 +85,50 @@ public class SingleSocketServer implements Runnable {
     public void run() {
         try {
             /* connection setups, read data from client. */
-            byte[] buf = new byte[1024];  // received data from client
-            int buf_len = -1;             // received data length
-            InputStream inputStream = client.getInputStream();
-
             InetAddress inetAddress = null;
             inetAddress = client.getInetAddress();
 
-            BufferedReader socketIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            InputStream inStrm_sock = client.getInputStream();
 
-            boolean flag = true;
-            while (flag) {
-                String msg = socketIn.readLine();
-                if (msg == null || "".equals(msg)) {
-                    break;  // or 'flag = false';
+            /* received buf. */
+            int predict_len = 24;
+            int monitor_nodes = 3;
+            int float_byte_size = 4;
+            byte[] receive = new byte[(monitor_nodes + predict_len) * float_byte_size];
+
+            /* received parsed data. */
+            float[] cur_nodes_trace_data = new float[monitor_nodes];
+            float[] predict_trace_data = new float[predict_len];
+
+            int i,j;
+            while (true) {
+
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                int len = inStrm_sock.read(receive, 0, receive.length);
+                System.out.println(df.format(new Date()) + ", len: " + len);
+
+                for (i = 0, j = 0; i < len; i = i + float_byte_size, j++) {
+                    if (j < monitor_nodes) {   // first 'monitor_nodes' points
+                        cur_nodes_trace_data[j] = bytes2float(receive, i);
+                        System.out.println("cur_nodes_trace_data[" + j + "]: " + cur_nodes_trace_data[j]);
+                        continue;
+                    }
+
+                    predict_trace_data[j-monitor_nodes] = bytes2float(receive, i);
+                    System.out.println("predict_trace_data[" + (j-monitor_nodes) + "]: " + predict_trace_data[j-monitor_nodes]);
+                }
+                System.out.println();
+
+                if (len < 0) {
+                    break;
                 }
 
-                /*JsonObject object = Json.parse(msg).asObject();
-                String name = object.get("name").asString();
-                System.out.println(name); */
-
-//                parse(msg);
-
-                System.out.println("server:" + msg);
-
+                Arrays.fill(cur_nodes_trace_data, 0);
+                Arrays.fill(predict_trace_data, 0);
+                Arrays.fill(receive, (byte) 0);
             }
 
-            socketIn.close();
+            inStrm_sock.close();
             client.close();
             socket_num--;
             System.out.println("client<" + inetAddress + "> disconnected! connected_num: " + socket_num);
@@ -94,6 +144,7 @@ public class SingleSocketServer implements Runnable {
             /* server */
             ServerSocket serverSocket = new ServerSocket(PORT);
             System.out.println("server socket is waiting to be connected ...");
+            System.out.println(serverSocket);
             System.out.println("listening port is " + PORT);
 
             Socket client = null;
